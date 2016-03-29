2c1bb2585e29aa99aa93606ee06401b9d1eaa7cb
Juyang8080 <juyang_08@163.com>
2016-03-28 21:38:34 -0700

    update first commit

--- httpd.h
+++ httpd.h
@@ -9,18 +9,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <netinet/in.h>
-//#include <strings.h>
 #include <errno.h>
 #include <arpa/inet.h>
 #include <pthread.h>
-//#include <ctype.h>
 #include <sys/sendfile.h>
-//#include <sys/wait.h>
 #include <unistd.h>
+#include <poll.h>
 int get_line(int sock,char* buf, int max_len);
 
 #define _BACKLOG_ 5
 #define _COMM_SIZE 1024
 #define MAIN_PAGE "index.html"
 #define HTTP_VERSION "HTTP/1.0"
+#define _DEF_VAL_ -1
 #endif
